bd38c51c8ee82bc649fc10c081e928473317fff1
OlgaSperrow <osperling17@gmail.com>
2025-03-08 12:35:39 +0100

    fix bug

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-function filterPrimes(arr) {
-  return arr.filter((e) => {
-    if (e < 2) return false;
-    for (let i = 2; i < e; i++) {
-      if (e % i === 0) return false;
-    }
-    return true;
-  });
-}
 
-console.log(filterPrimes([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
 
 const array = [10, 15, 8, 3, 6];
 //function changed original because I use copy of array
