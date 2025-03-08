cf914f635b925f811b277621422e1b95f36306d0
HSushynska <stoyanova.gv@gmail.com>
2025-03-08 18:35:40 +0100

    add feature/most-frequent

--- script.js
+++ script.js
@@ -37,4 +37,23 @@ const sortedArray = sortByDivisors(array);
 console.log ("sorted array:", sortedArray);
 console.log( "original:", array);
 
+function mostFrequent(arr) {
+  const frequent = {};
+  let maxFrequent = 0;
+  let mostFrequentEl = null; 
+
+  for (const element of arr) {
+    frequent[element] = (frequent[element] || 0) + 1;
+    if (frequent[element] > maxFrequent) {
+      maxFrequent = frequent[element];
+      mostFrequentEl = element;
+
+    }
+  }
+  return mostFrequentEl;
+}
+
+console.log(mostFrequent([1, 2, 2, 3, 3, 3, 4]));
+     
+
 
