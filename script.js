9aae53acd9f964cd24d1a0564dcca6231e65d280
OlgaSperrow <Osperling17@gmail.com>
2025-03-08 20:02:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -78,15 +78,4 @@ console.log(groupByParity([1, 2, 3, 4, 5, 6]));
 
 
 
-  function filterPrimes(arr) {
-    return arr.filter((e) => {
-      if (e < 2) return false;
-      for (let i = 2; i < e; i++) {
-        if (e % i === 0) return false;
-      }
-      return true;
-    });
-  }
-  
-  console.log(filterPrimes([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
