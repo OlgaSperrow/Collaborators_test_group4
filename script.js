2fcd60025dffe0d6fb5ecb8765148f157bdf91fc
OlgaSperrow <Osperling17@gmail.com>
2025-03-08 20:22:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-
-function removeDuplicates(arr) {
-    return [...new Set(arr)];
-
-  
-  console.log(filterPrimes([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-  
-
-
-
-
-
-const array = [10, 15, 8, 3, 6];
-//function changed original because I use copy of array
-function sortByDivisors(arr) {
-  return [...arr].sort((a, b) => {
-    let numberA = 0;
-    let numberB = 0;
-
-    for (let i = 1; i <= a; i++) {
-      if (a % i === 0) numberA++;
-    }
-    for (let i = 1; i <= b; i++) {
-      if (b % i === 0) numberB++;
-    }
-    return numberA-numberB;
-  });
-}
-
-const sortedArray = sortByDivisors(array);
-console.log ("sorted array:", sortedArray);
-console.log( "original:", array);
-
 function mostFrequent(arr) {
   const frequent = {};
   let maxFrequent = 0;
@@ -50,7 +17,4 @@ function mostFrequent(arr) {
 console.log(mostFrequent([1, 2, 2, 3, 3, 3, 4]));
      
 
-
-  const arr = [1, 2, 2, 3, 4, 4, 5];
-  console.log(removeDuplicates(arr));
 
