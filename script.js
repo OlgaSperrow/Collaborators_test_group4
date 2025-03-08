891de95e78fdbff9348fd0d7c3f22e86918d3c15
OlgaSperrow <osperling17@gmail.com>
2025-03-08 19:12:50 +0100

    function sortByDivisors removed

--- script.js
+++ script.js
@@ -16,25 +16,4 @@ function filterPrimes(arr) {
 
 
 
-const array = [10, 15, 8, 3, 6];
-//function changed original because I use copy of array
-function sortByDivisors(arr) {
-  return [...arr].sort((a, b) => {
-    let numberA = 0;
-    let numberB = 0;
-
-    for (let i = 1; i <= a; i++) {
-      if (a % i === 0) numberA++;
-    }
-    for (let i = 1; i <= b; i++) {
-      if (b % i === 0) numberB++;
-    }
-    return numberA-numberB;
-  });
-}
-
-const sortedArray = sortByDivisors(array);
-console.log ("sorted array:", sortedArray);
-console.log( "original:", array);
-
 
